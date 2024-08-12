a82a53c2ad06fbbf4160c20a6dda1c0a57ea9247
Vatsalya Bajpai <vatsalyabajpai03@gmail.com>
2024-08-12 22:30:08 +0530

    Reading file line by line, took 13m6.5s

--- calculate_metrics_cm.py
+++ calculate_metrics_cm.py
@@ -3,12 +3,11 @@ def calculate():
     _max = -100
     result = {}
     with open("measurements.txt", "r", encoding="utf-8") as f:
-        lines = f.readlines()
-    for line in lines:
-        station_val = line.split(';')
-        val = float(station_val[1])
-        curr_min, curr_sum, curr_max, curr_count = result.get(station_val[0], (_min, 0, _max, 0))
-        result[station_val[0]] = (min(val, curr_min), curr_sum + val, max(curr_max, val), curr_count + 1)
+        for line in f:
+            station_val = line.split(';')
+            val = float(station_val[1])
+            curr_min, curr_sum, curr_max, curr_count = result.get(station_val[0], (_min, 0, _max, 0))
+            result[station_val[0]] = (min(val, curr_min), curr_sum + val, max(curr_max, val), curr_count + 1)
 
     print("{", end="")
     for k, v in result.items():
